f4226fd6f52c3a5901c911ff60e8df1a5942f202
alina-muellenmeister <alina.muellenmeister@burnet.edu.au>
2024-04-18 16:17:28 +1000

    HIV: Updates to viral dynamics and inclusion of ART treatment with effect on viral dynamics

--- run_hiv.py
+++ run_hiv.py
@@ -10,9 +10,10 @@ import matplotlib.pyplot as plt
 import sciris as sc
 from stisim.networks import StructuredSexual
 from stisim.diseases.hiv import HIV
+from stisim.interventions import ART
 
 quick_run = False
-ss.options['multirng']=False
+ss.options['multirng'] = False
 
 
 def make_hiv_sim(location='zimbabwe', total_pop=100e6, dt=1, n_agents=500, latent_trans=0.075):
@@ -20,9 +21,8 @@ def make_hiv_sim(location='zimbabwe', total_pop=100e6, dt=1, n_agents=500, laten
     hiv = HIV()
     hiv.pars['beta'] = {'structuredsexual': [0.95, 0.5], 'maternal': [0.99, 0]}
     hiv.pars['init_prev'] = ss.bernoulli(p=0.1)
-    #hiv.pars['rel_trans']['latent_temp'] = latent_trans
-    #hiv.pars['rel_trans']['latent_long'] = latent_trans
-
+    # hiv.pars['rel_trans']['latent_temp'] = latent_trans
+    # hiv.pars['rel_trans']['latent_long'] = latent_trans
 
     # Make demographic modules
     fertility_rates = {'fertility_rate': pd.read_csv(f'data/{location}_asfr.csv')}
@@ -44,6 +44,7 @@ def make_hiv_sim(location='zimbabwe', total_pop=100e6, dt=1, n_agents=500, laten
         people=ppl,
         diseases=hiv,
         networks=ss.ndict(sexual, maternal),
+        interventions=ART(year=1990, coverage=0.5),
         demographics=[pregnancy, death],
     )
 
@@ -51,7 +52,6 @@ def make_hiv_sim(location='zimbabwe', total_pop=100e6, dt=1, n_agents=500, laten
 
 
 def run_hiv(location='zimbabwe', total_pop=100e6, dt=1.0, n_agents=500):
-
     sim_kwargs = make_hiv_sim(location=location, total_pop=total_pop, dt=dt, n_agents=n_agents)
     sim = ss.Sim(**sim_kwargs)
     sim.run()
@@ -59,20 +59,71 @@ def run_hiv(location='zimbabwe', total_pop=100e6, dt=1.0, n_agents=500):
     df = pd.DataFrame.from_dict(df_res)
     return sim, df
 
-def plot_cd4_count(sim_output):
-    '''
-    Add a plot of time vs CD4 count for a subset of agents to make sure CD4 counts work sensibly.
-    '''
-
+def plot_T_level(sim_output):
+    """
+    Single plot of CD4 counts
+    """
     fig, ax = plt.subplots(1, 1, figsize=(5, 4))
-    for agent in np.arange(0, 5):
-        plt.plot(sim_output.index, sim_output['hiv.Agent' + str(agent) + '_cd4'])
-
-    ax.set_xlabel('Time')
-    ax.set_ylabel('CD4 count (cells/uL)')
-    ax.set_title('CD4 counts')
+    # Plot the first 40 days only:
+    sim_output_sub = sim_output.iloc[0:40]
+    plt.plot(np.arange(0, 40, 1), sim_output_sub['hiv.Agent' + str(11) + '_T_level'])
+    ax.set_xlabel('Time (days)')
+    ax.set_ylabel('CD4-count (cells/uL)')
+    ax.set_title('CD4-count (cells/uL)')
     fig.tight_layout()
-    sc.savefig("figures/cd4_count.png", dpi=100)
+    sc.savefig("figures/cd4_counts.png", dpi=100)
+
+
+def plot_V_level(sim_output):
+    """
+    Single Plot of viral load
+    """
+    fig, ax = plt.subplots(1, 1, figsize=(5, 4))
+    # Plot the first 40 days only:
+    sim_output_sub = sim_output.iloc[0:40]
+    plt.plot(np.arange(0, 40, 1), sim_output_sub['hiv.Agent' + str(11) + '_V_level'])
+    ax.set_xlabel('Time (days)')
+    ax.set_yscale('log')
+    ax.set_ylabel('Viral load (copies/mL)')
+    ax.set_title('Viral load (copies/mL)')
+    fig.tight_layout()
+    sc.savefig("figures/viral_loads.png", dpi=100)
+
+
+def plot_viral_dynamics(sim_output):
+    """
+    3 Subplots to show viral load, CD4 count and infected cells count
+    """
+    fig, ax = plt.subplots(1, 3, figsize=(10, 4))
+    # Plot the first 40 days only:
+    sim_output_sub = sim_output.iloc[0:40]
+
+    # Viral Load:
+    ax[0].plot(np.arange(0, 40, 1), sim_output_sub['hiv.Agent' + str(11) + '_V_level'] * 1e3, label='on ART after 30 days')
+    ax[0].plot(np.arange(0, 40, 1), sim_output_sub['hiv.Agent' + str(36) + '_V_level'] * 1e3, label='not on ART')
+    ax[0].set_xlabel('Time (days)')
+    ax[0].set_yscale('log')
+    ax[0].set_ylabel('Viral load (copies/mL)')
+    ax[0].set_title('Viral load')
+
+    # CD4-count
+    ax[1].plot(np.arange(0, 40, 1), sim_output_sub['hiv.Agent' + str(11) + '_T_level'], label='on ART after 30 days')
+    ax[1].plot(np.arange(0, 40, 1), sim_output_sub['hiv.Agent' + str(36) + '_T_level'], label='not on ART')
+    ax[1].set_xlabel('Time (days)')
+    ax[1].set_ylabel('CD4-count (cells/uL)')
+    ax[1].set_title('CD4-count')
+
+    # Infected cells count:
+    ax[2].plot(np.arange(0, 40, 1), sim_output_sub['hiv.Agent' + str(11) + '_I_level'], label='on ART after 30 days')
+    ax[2].plot(np.arange(0, 40, 1), sim_output_sub['hiv.Agent' + str(36) + '_I_level'], label='not on ART')
+    ax[2].set_xlabel('Time (days)')
+    ax[2].set_ylabel('Infected cell count (cells/uL)')
+    ax[2].set_title('Infected cells')
+
+    ax[0].legend()
+    fig.tight_layout()
+    sc.savefig("figures/viral_dynamics.png", dpi=100)
+
 
 if __name__ == '__main__':
 
@@ -81,11 +132,13 @@ if __name__ == '__main__':
         nigeria=93963392,
         zimbabwe=9980999,
     )[location]
-    sim, output = run_hiv(location=location, total_pop=total_pop, dt=1/365, n_agents=int(10e3))
+    sim, output = run_hiv(location=location, total_pop=total_pop, dt=1 / 365, n_agents=int(10e3))
     output.to_csv("HIV_output.csv")
 
-    # plot_cd4_count(output)
+    # Plot viral dynamics of one infected agent not on ART (hard-coded) to check if dynamics work sensibly
+    if sim.dt == 1 / 365:
+        # plot_T_level(output)
+        # plot_V_level(output)
+        plot_viral_dynamics(output)
 
     sc.saveobj(f'sim_{location}.obj', sim)
-
-
--- stisim/diseases/hiv.py
+++ stisim/diseases/hiv.py
@@ -6,7 +6,7 @@ import numpy as np
 import sciris as sc
 import starsim as ss
 
-__all__ = ['HIV', 'ART', 'CD4_analyzer']
+__all__ = ['HIV']
 
 
 class HIV(ss.Infection):
@@ -16,10 +16,10 @@ class HIV(ss.Infection):
         self.add_states(
             ss.State('on_art', bool, False),
             ss.State('ti_art', int, ss.INT_NAN),
-            ss.State('cd4', float, 500),  # cells/uL
-            ss.State('T', float, 1000),  #
-            ss.State('I', float, 1e-3),  # cells per uL
-            ss.State('V', float, 0),  #
+            # ss.State('cd4', float, 500),  # cells/uL
+            ss.State('cd4', float, 1000),  #
+            ss.State('infected_cells', float, 1e-3),  # cells per uL
+            ss.State('virus', float, 0),  #
             ss.State('viral_load', float, 1e7),  # RNA copies/mL
             ss.State('ti_dead', int, ss.INT_NAN),  # Time of HIV-cause death
         )
@@ -27,10 +27,13 @@ class HIV(ss.Infection):
         pars = ss.omergeleft(pars,
                              cd4_min=100,
                              cd4_max=500,
-                             cd4_rate=5,
-                             viral_load_min=1e2,
-                             viral_load_max=1e6,
-                             viral_load_rate_= 5,
+                             # From: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6155466/#imr12698-bib-0019
+                             d_T=0.1 / 24,  # 0.1 cells/uL/day
+                             lambda_T=100 / 24,  # 100 cells/uL/day
+                             d_I=1 / 24,  # 1 cell/uL/day
+                             k=250 / 24,  # 250 virus/cell/day
+                             c=25 / 24,  # 25 virus/day
+                             beta_noART=(3 * 1e-4) / 24,  # 3 * 1e-7 virus/ml/day -> 3 * 1e-4 virus/uL/day
                              init_prev=0.05,
                              eff_condoms=0.7,
                              art_efficacy=0.96,
@@ -56,37 +59,39 @@ class HIV(ss.Infection):
         return out
 
     def update_pre(self, sim):
-        # Update CD4 count
-        infected_and_on_art_uids = sim.people.alive & self.infected & self.on_art
-        infected_and_not_on_art_uids = sim.people.alive & self.infected & ~self.on_art
-        self.cd4[infected_and_on_art_uids] += (self.pars.cd4_max - self.cd4[infected_and_on_art_uids]) / self.pars.cd4_rate
-        self.cd4[infected_and_not_on_art_uids] += (self.pars.cd4_min - self.cd4[infected_and_not_on_art_uids]) / self.pars.cd4_rate
+        """
+        Update viral dynamics
+        """
 
-        d_T = 0.1 # 0.1 per day
-        lambda_T = 100  # 100 per day
-        d_I = 1  # 1 per day
-        k = 250  # 250 per day
-        c = 0.25  # 25 per day
-        beta_noART = 3 * 1e-4  # 3 * 10^-7 ml per day
+        infected_uids = sim.people.alive & self.infected
 
         if sim.ti > 0:
+            current_uninfected_cells = self.cd4[infected_uids]
+            current_infected_cells = self.infected_cells[infected_uids]
+            current_virus = self.virus[infected_uids]
+            # E.g. If time step is 1-day, this will update the viral dynamics over 24hrs.
+            for hour in np.arange(0, 8760 * sim.dt, 1):
+                # Update beta for agents on ART
+                use_beta = [self.pars.beta_noART * (1-self.pars.art_efficacy) if self.on_art[uid] else self.pars.beta_noART
+                            for uid in current_uninfected_cells.uid]
 
-            new_uninfected_cells = lambda_T - beta_noART * self.V[infected_and_not_on_art_uids] * \
-                                   self.T[infected_and_not_on_art_uids] - self.T[infected_and_not_on_art_uids] * d_T
+                # Calculate the CD4 and infected cell counts, and the viral load changes
+                new_uninfected_cells = self.pars.lambda_T - use_beta * current_uninfected_cells * current_virus - current_uninfected_cells * self.pars.d_T
+                new_infected_cells = use_beta * current_uninfected_cells * current_virus - current_infected_cells * self.pars.d_I
+                new_virus = self.pars.k * current_infected_cells - current_virus * self.pars.c
 
-            new_infected_cells = beta_noART * self.T[infected_and_not_on_art_uids] * \
-                                 self.V[infected_and_not_on_art_uids] - self.I[infected_and_not_on_art_uids] * d_I
+                # Update free virus
+                current_virus += new_virus
+                # Update infected cells
+                current_infected_cells += new_infected_cells
+                # Update uninfected target cells
+                current_uninfected_cells += new_uninfected_cells
 
-            new_virus = k * self.I[infected_and_not_on_art_uids] - self.V[infected_and_not_on_art_uids] * c
+            # Update values at the end of the time step
+            self.cd4[infected_uids] = current_uninfected_cells
+            self.infected_cells[infected_uids] = current_infected_cells
+            self.virus[infected_uids] = current_virus
 
-            # Update free virus
-            self.V[infected_and_not_on_art_uids] += new_virus
-            # Update infected cells
-            self.I[infected_and_not_on_art_uids] += new_infected_cells
-            # Update uninfected target cells
-            self.T[infected_and_not_on_art_uids] += new_uninfected_cells
-
-        print(sim.ti, self.T[11], self.I[11], self.V[11])
         self.rel_trans[sim.people.alive & self.infected & self.on_art] = 1 - self.pars['art_efficacy']
 
         can_die = ss.true(sim.people.alive & sim.people.hiv.infected)
@@ -102,19 +107,28 @@ class HIV(ss.Infection):
         """
         super().init_results(sim)
         self.results += ss.Result(self.name, 'new_deaths', sim.npts, dtype=int)
-        # Adding this for now to check if CD4 counts and viral loads work sensible
-        for agent in np.arange(10, 12, 1):
-            self.results += ss.Result(self.name, 'Agent' + str(agent) + '_cd4', sim.npts, dtype=int)
+        # Save the V, T and I level for a few example agents not on Art:
+        # infected_and_on_art_uids = [uid for uid, flag in enumerate(sim.people.alive & self.infected & self.on_art) if flag]
+        # infected_and_not_on_art_uids = [uid for uid, flag in enumerate(sim.people.alive & self.infected & ~self.on_art) if flag]
+        for agent in [11, 36]:
             self.results += ss.Result(self.name, 'Agent' + str(agent) + '_V_level', sim.npts, dtype=int)
+            self.results += ss.Result(self.name, 'Agent' + str(agent) + '_T_level', sim.npts, dtype=int)
+            self.results += ss.Result(self.name, 'Agent' + str(agent) + '_I_level', sim.npts, dtype=int)
         return
 
     def update_results(self, sim):
         super().update_results(sim)
         self.results['new_deaths'][sim.ti] = np.count_nonzero(self.ti_dead == sim.ti)
-        # Adding this for now to check if CD4 counts and viral loads work sensible
-        for agent in np.arange(10, 12, 1):
-            self.results['Agent' + str(agent) + '_cd4'][sim.ti] = self.cd4[agent]
-            self.results['Agent' + str(agent) + '_V_level'][sim.ti] = self.V[agent].astype(dtype=np.int64) / 1e7
+        # Save the V, T and I level for two example agents:
+        # infected_and_on_art_uids = [uid for uid, flag in enumerate(sim.people.alive & self.infected & self.on_art) if
+        #                             flag]
+        # infected_and_not_on_art_uids = [uid for uid, flag in enumerate(sim.people.alive & self.infected & ~self.on_art)
+        #                                 if flag]
+        for agent in [11, 36]:
+            # Save the V, T and I level for two example agents:
+            self.results['Agent' + str(agent) + '_V_level'][sim.ti] = round(self.virus[agent].astype(dtype=np.int64), 4)
+            self.results['Agent' + str(agent) + '_T_level'][sim.ti] = round(self.cd4[agent].astype(dtype=np.int64), 4)
+            self.results['Agent' + str(agent) + '_I_level'][sim.ti] = round(self.infected_cells[agent].astype(dtype=np.int64), 4)
         return
 
     def make_new_cases(self, sim):
@@ -132,63 +146,3 @@ class HIV(ss.Infection):
 
     def set_congenital(self, sim, target_uids, source_uids):
         return self.set_prognoses(sim, target_uids, source_uids)
-
-
-# %% HIV-related interventions
-
-class ART(ss.Intervention):
-
-    def __init__(self, year: np.array, coverage: np.array, **kwargs):
-        self.requires = HIV
-        self.year = sc.promotetoarray(year)
-        self.coverage = sc.promotetoarray(coverage)
-
-        super().__init__(**kwargs)
-
-        self.prob_art_at_infection = ss.bernoulli(
-            p=lambda self, sim, uids: np.interp(sim.year, self.year, self.coverage))
-        return
-
-    def initialize(self, sim):
-        super().initialize(sim)
-        self.results += ss.Result(self.name, 'n_art', sim.npts, dtype=int)
-        self.initialized = True
-        return
-
-    def apply(self, sim):
-        if sim.year < self.year[0]:
-            return
-
-        ti_delay = 1  # 1 time step delay TODO
-        recently_infected = ss.true((sim.people.hiv.ti_infected == sim.ti - ti_delay) & sim.people.alive)
-
-        n_added = 0
-        if len(recently_infected) > 0:
-            inds = self.prob_art_at_infection.filter(recently_infected)
-            sim.people.hiv.on_art[inds] = True
-            sim.people.hiv.ti_art[inds] = sim.ti
-            n_added = len(inds)
-
-        # Add result
-        self.results['n_art'][sim.ti] = np.count_nonzero(sim.people.alive & sim.people.hiv.on_art)
-
-        return n_added
-
-
-# %% Analyzers
-
-class CD4_analyzer(ss.Analyzer):
-
-    def __init__(self):
-        self.requires = HIV
-        self.cd4 = None
-        return
-
-    def initialize(self, sim):
-        super().initialize(sim)
-        self.cd4 = np.zeros((sim.npts, sim.people.n), dtype=int)
-        return
-
-    def apply(self, sim):
-        self.cd4[sim.t] = sim.people.hiv.cd4
-        return
