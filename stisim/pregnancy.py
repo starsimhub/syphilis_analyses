884e516086827fb255f7a5dd0154f96696fb32ca
Robyn Stuart <robyna.s@gmail.com>
2024-04-24 14:34:21 +1000

    add template

---
+++ stisim/pregnancy.py
@@ -0,0 +1,14 @@
+"""
+Define pregnancy
+"""
+
+import starsim as ss
+# import numpy as np
+# import sciris as sc
+# import pandas as pd
+
+__all__ = ['Conception']
+
+
+class Conception(ss.Demographics):
+    pass
