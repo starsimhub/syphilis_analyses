90fa6a23da44ab94b9e0e1e10dcfcd68355cebdc
Romesh Abeysuriya <romesh.abey@gmail.com>
2024-05-22 09:32:24 +1000

    Tidy test

--- tests/devtest_hiv_syph.py
+++ tests/devtest_hiv_syph.py
@@ -5,25 +5,6 @@ import pandas as pd
 import numpy as np
 
 np.seterr(all='raise')
-#
-# location = 'zimbabwe'
-# fertility_rates = {'fertility_rate': pd.read_csv(sti.data / f'{location}_asfr.csv')}
-# death_rates = {'death_rate': pd.read_csv(sti.data / f'{location}_deaths.csv'), 'units': 1}
-#
-# pars = {}
-# pars['n_agents'] = 500
-#
-# hiv = sti.HIV(init_prev=0.15, beta={'structuredsexual': [0.3, 0.15], 'maternal': [0.95, 0.0]})
-# syphilis = sti.SyphilisPlaceholder(prevalence=0.2)
-#
-# pars['diseases'] = [hiv, syphilis]
-# pars['connectors'] = sti.hiv_syph(hiv, syphilis)
-# pars['networks'] = [sti.StructuredSexual(), ss.MaternalNet()]
-# pars['demographics'] = [ss.Pregnancy(pars=fertility_rates), ss.Deaths(death_rates)]
-#
-# s0 = ss.Sim(pars).run()
-
-
 
 location = 'zimbabwe'
 fertility_rates = {'fertility_rate': pd.read_csv(sti.data / f'{location}_asfr.csv')}
